4f9db2ec661b22b41ecf94265d58d49e59b97aba
Gilad Nadav <gilad.nadav@kaltura.com>
2018-09-16 10:09:43 +0300

    cosmetics

--- googlecast/src/main/java/com/kaltura/playkit/plugins/googlecast/GenericCastInfo.java
+++ googlecast/src/main/java/com/kaltura/playkit/plugins/googlecast/GenericCastInfo.java
@@ -1,5 +1,17 @@
 package com.kaltura.playkit.plugins.googlecast;
 
+/*
+ * ============================================================================
+ * Copyright (C) 2017 Kaltura Inc.
+ *
+ * Licensed under the AGPLv3 license, unless a different license for a
+ * particular library is specified in the applicable library path.
+ *
+ * You may obtain a copy of the License at
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ * ============================================================================
+ */
+
 import android.text.TextUtils;
 
 import org.json.JSONException;
