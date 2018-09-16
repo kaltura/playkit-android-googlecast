b347d83b3de1f0759d36be8afdde8562358856af
Gilad Nadav <gilad.nadav@kaltura.com>
2018-09-16 10:06:54 +0300

    V3 cast integration

---
+++ googlecast/src/main/java/com/kaltura/playkit/plugins/googlecast/GenericCastInfo.java
@@ -0,0 +1,74 @@
+package com.kaltura.playkit.plugins.googlecast;
+
+import android.text.TextUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GenericCastInfo {
+    private String mediaEntryId;
+    private String ks;
+    private String textLanguage;
+    private String audioLanguage;
+
+    public GenericCastInfo() {}
+
+    public String getMediaEntryId() {
+        return mediaEntryId;
+    }
+
+    public GenericCastInfo setMediaEntryId(String mediaEntryId) {
+        this.mediaEntryId = mediaEntryId;
+        return this;
+    }
+
+    public String getKs() {
+        return ks;
+    }
+
+    public GenericCastInfo setKs(String ks) {
+        this.ks = ks;
+        return this;
+    }
+
+    public String getTextLanguage() {
+        return textLanguage;
+    }
+
+    public GenericCastInfo setTextLanguage(String textLanguage) {
+        this.textLanguage = textLanguage;
+        return this;
+    }
+
+    public String getAudioLanguage() {
+        return audioLanguage;
+    }
+
+    public GenericCastInfo setAudioLanguage(String audioLanguage) {
+        this.audioLanguage = audioLanguage;
+        return this;
+    }
+
+    public JSONObject getCastInfoAsJson() {
+        JSONObject mediaInfo = new JSONObject();
+        try {
+
+            JSONObject customData = new JSONObject();
+            customData.put("entryId", getMediaEntryId());
+            if (!TextUtils.isEmpty(getKs())) {
+                customData.put("ks", getKs());
+            }
+            if (!TextUtils.isEmpty(getTextLanguage())) {
+                customData.put("textLanguage", getTextLanguage());
+            }
+            if (!TextUtils.isEmpty(getAudioLanguage())) {
+                customData.put("audioLanguage", getAudioLanguage());
+            }
+            mediaInfo.put("mediaInfo", customData);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return mediaInfo;
+    }
+}
+
